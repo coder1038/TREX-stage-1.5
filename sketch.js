97797143d3dcd732d57d9fb54e23c6de4ac31f3c
coder1038 <94860389+coder1038@users.noreply.github.com>
2021-11-24 18:14:49 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+var trex ,trex_running;
+function preload(){
+ trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex=createSprite(50,160,30,80)
+ trex.addAnimation("running",trex_running)
+ trex.scale=0.5
+}
+
+function draw(){
+  background("black")
+  if (keyDown=("space")){
+ trex.velocityY=-10
+}
+trex.velocityY=trex.velocityY+0.5
+trex.collide(edges[3])
+drawSprites()
+
+}
